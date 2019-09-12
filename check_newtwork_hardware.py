94b604f5401a52c9aa7e5da96c82c3fe6f40f2e2
Evgeny <Evgeny_Mrykhin@epam.com>
2019-09-12 13:36:29 +0300

    add time_module

--- check_newtwork_hardware.py
+++ check_newtwork_hardware.py
@@ -1,11 +1,15 @@
+# -*- coding: UTF-8 -*-
 import paramiko
+import datetime
+import time
 
 
-class SSH:
+class SSH():
     def __init__(self, **kwargs):
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.kwargs = kwargs
+
 
     def __enter__(self):
         '''Метод подключения к удаленному хосту с импрортируемым модулем paramiko'''
@@ -17,17 +21,24 @@ class SSH:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.client.close()
 
+    def timeline(self, st):
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S')
+        self.st = st
+        return self.st
+
     def exec_cmd(self, cmd):
         ''' Необходимо выполнить команду с помощью скрипта (interface print)'''
         stdin, stdout, stderr = self.client.exec_command(cmd)
         data = stdout.read() + stderr.read()
-        if not stderr:
-            raise stderr
         return data.decode()
 
 
 if __name__ == '__main__':
-    with SSH(hostname='10.10.10.254', username='usver', password='passwd', port=22) as ssh:  # usver@10.10.10.254
-        out = ssh.exec_cmd('interface print')
+    with SSH(hostname='10.20.10.254', username='usver', passwd', port=22) as ssh:  # usver@10.20.10.254
+        out = ssh.exec_cmd('ip firewall filter print')   # команда выполняемая на устройстве
+        res = ssh.timeline('%d-%m-%Y %H:%M:%S')   # формат времени
         print(out)
-        print(out, file=open('D:\\tasks\\log.txt', 'a'))  # и записью вывода в лог
+        print('  =========>> Time is: ' + res + ' ' + '<<=========' + '\n' + ' ' + '\n', out + '\n' +
+              '//-------------------------------------------------------------------//',
+              file=open('D:\\tasks\\log.txt', 'a'))  # и записью вывода в лог
