5a4288e843a3b99218755debb0c661428d910101
Evgeny <Evgeny_Mrykhin@epam.com>
2019-10-07 17:03:48 +0300

    add update output for pars2

--- pars2.py
+++ pars2.py
@@ -1,12 +1,14 @@
 import os
 
-fw = "D:\\logs"
-word = 'error'
+fw = "D:\\configs"
+word = 'route'
 
+if os.path.exists(fw):
+    for root, dir, files in os.walk(fw):
+        for file in files:
+            with open(fw+"\\"+file) as log:
+                for line in log.readlines():
+                    if word in line:
+                        print(file+" ---> "+line)
 
-for root, dir, files in os.walk(fw):
-    for file in files:
-        with open(fw+"\\"+file) as log:
-            for line in log.readlines():
-                if word in line:
-                    print(file+" ---> "+line)
+raise ValueError("Not found dir")
