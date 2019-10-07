f3bf86e2496a00e666313688934378134ed8137e
Evgeny <Evgeny_Mrykhin@epam.com>
2019-10-07 16:39:16 +0300

    add new script

---
+++ pars2.py
@@ -0,0 +1,12 @@
+import os
+
+fw = "D:\\logs"
+word = 'error'
+
+
+for root, dir, files in os.walk(fw):
+    for file in files:
+        with open(fw+"\\"+file) as log:
+            for line in log.readlines():
+                if word in line:
+                    print(line)
