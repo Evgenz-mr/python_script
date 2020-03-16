3869d5d3aa1b0ce7904008f31e8efb8dcd2e74d8
Evgenz-mr <45653581+Evgenz-mr@users.noreply.github.com>
2020-03-16 09:41:12 +0300

    update to cred_calc
    update to cred_calc

--- cred_calc.py
+++ cred_calc.py
@@ -1,48 +1,6 @@
-# import pandas as pd
-#
-# # intialise data of lists.
-# data = {'Name': ['Tom', 'nick', 'krish', 'jack', 'Evgen'],
-#         'Age': [a for a in range(20, 25)]}
-#
-# # Create DataFrame
-# df = pd.DataFrame(data)
-#
-# # Print the output.
-# print(df)
-
-
-####
-####
-# Import pandas package
-# import pandas as pd
-#
-# # Define a dictionary containing employee data
-# data = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
-#         'Age': [27, 24, 22, 32],
-#         'Address': ['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
-#         'Qualification': ['Msc', 'MA', 'MCA', 'Phd']}
-#
-# # Convert the dictionary into DataFrame
-# df = pd.DataFrame(data)
-#
-# # select two columns
-# print(df)
-
-
 ######
 ######
 # расчет переплаты процентов за кредит
-
-# import webbrowser
-# import re
-#
-# call = input('Введите ссылку или запрос: ')
-# if re.search(r'\.', call):
-#     webbrowser.open_new_tab('https://' + call)
-# elif re.search(r'\ ', call):
-#     webbrowser.open_new_tab('https://yandex.ru/search/?text='+call)
-# else:
-#     webbrowser.open_new_tab('https://yandex.ru/search/?text='+call)
 
 def credit():
     cred = float(input("Введите сумму стоимости автомобиля: "))
@@ -70,42 +28,3 @@ while True:
     ex = input("Хотите повторить оперцию ?: ")
     if ex not in ans:
         break
-
-# import sys
-#
-# message = ' '.join(sys.argv[1:]) or "Hello World!"
-# channel.basic_publish(exchange='',
-#                       routing_key='hello',
-#                       body=message)
-# print(" [x] Sent %r" % message)
-
-# import requests
-# from bs4 import BeautifulSoup
-# import csv
-# # pip install beautifulsoup4
-# # pip install lxml
-#
-# def get_html(url):
-#     r = requests.get(url)    # Получим метод Response
-#     r.encoding = 'utf8'
-#     return r.text   # Вернем данные объекта text
-#
-#
-# def csv_read(data):
-#     with open("data.csv", 'a') as file:
-#         writer = csv.writer(file)
-#         writer.writerow((data['head'], data['link']))
-#
-# def get_link(html):
-#     soup = BeautifulSoup(html, 'lxml')
-#     head = soup.find('div', id='section-content').find_all('a', class_="entry-header")
-#     for i in head:
-#         link = 'https://ru.investing.com/equities/gazprom_rts' + i.get('href')
-#         heads= i.find('h1').string
-#         data = {'head': heads,
-#                  'link': link}
-#         csv_read(data)
-#
-#
-# data = get_link(get_html('https://ru.investing.com/equities/gazprom_rts'))
-# #https://3dnews.ru/news
